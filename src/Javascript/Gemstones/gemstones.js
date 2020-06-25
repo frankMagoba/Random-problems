2de08b4aff06ca788d8d228e6cb453d0d8cade57
frankmagoba <franklinmagoba4@gmail.com>
2020-06-25 13:28:41 +0300

    gemstones solution

---
+++ src/Javascript/Gemstones/gemstones.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the gemstones function below.
+function gemstones(arr) {
+
+    let combined = arr.join('');
+    let unique = [...new Set(combined)];
+    let result = unique.filter(ch =>
+        arr.every(str => str.includes(ch)));
+    return result.length;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        const arrItem = readLine();
+        arr.push(arrItem);
+    }
+
+    let result = gemstones(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
